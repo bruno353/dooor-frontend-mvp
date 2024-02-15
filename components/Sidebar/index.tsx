903231d8a3b0e8f79f1b0243c118b2568b5ae3fb
Bruno Santos <brunolsantos152@gmail.com>
2024-02-15 20:33:33 -0300

    feat: fixing new css

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -71,66 +71,18 @@ const Sidebar = ({ onValueChange }) => {
     }
   }, [user])
 
-  if (!isOpen) {
-    return (
-      <>
-        <div
-          onMouseEnter={() => setIsOpen(true)}
-          className="z-50 h-full shadow-[0_4px_4px_0px_rgba(0,0,0,0.25)] lg:w-[125px] lg:max-w-[125px]"
-        >
-          <div className="flex flex-col items-center justify-center px-[15px] pb-[45px] pt-[49px]   lg:px-[15px] lg:pb-[90px] lg:pt-[98px]">
-            <div className="absolute top-[14px] flex w-[10.5px] cursor-pointer flex-col items-center lg:top-[38px] lg:w-[21.5px]">
-              <img
-                onClick={() => setIsOpen(true)}
-                src={`${
-                  process.env.NEXT_PUBLIC_ENVIRONMENT === 'PROD'
-                    ? process.env.NEXT_PUBLIC_BASE_PATH
-                    : ''
-                }/images/lateralNavBar/nav.svg`}
-                alt="image"
-              />
-            </div>
-            <div className="absolute top-[46px] flex w-[10.5px] cursor-pointer flex-col items-center lg:top-[92px] lg:w-[21.5px]">
-              <img
-                src={`${
-                  process.env.NEXT_PUBLIC_ENVIRONMENT === 'PROD'
-                    ? process.env.NEXT_PUBLIC_BASE_PATH
-                    : ''
-                }/images/logo/search.svg`}
-                alt="image"
-              />
-            </div>
-          </div>
-          <div className="mt-[px] grid gap-y-[10px] px-[22px] text-[13px] text-[#000]">
-            {pythiaChats &&
-              pythiaChats.map((chat, index) => (
-                <div
-                  key={index}
-                  onClick={() => {
-                    sendToChat(chat.id)
-                  }}
-                  className={`${
-                    pythiaChat && pythiaChat.id === chat.id
-                      ? 'bg-[#e2e2e25d]'
-                      : ''
-                  } cursor-pointer overflow-hidden truncate text-ellipsis whitespace-nowrap rounded-md p-[10px] hover:bg-[#e2e2e25d]`}
-                >
-                  Chat {chat.id}
-                </div>
-              ))}
-          </div>
-        </div>
-      </>
-    )
-  }
-
   return (
     <>
       <div
         onMouseLeave={() => setIsOpen(false)}
+        onMouseEnter={() => setIsOpen(true)}
         className="relative z-50 h-full shadow-[0_4px_4px_0px_rgba(0,0,0,0.25)]"
       >
-        <div className="flex w-[280px] flex-col items-start">
+        <div
+          className={`flex  flex-col items-start ${
+            isOpen ? 'w-[280px]' : 'w-[150px]'
+          }`}
+        >
           <div className="mb-[14.5px] mt-[24.5px] ml-[16px]  flex flex-row items-center  justify-between lg:mt-[49px] lg:mb-[29px] lg:ml-[32px]">
             <div className="absolute top-[46px] flex w-[10.5px] cursor-pointer flex-col items-center lg:top-[38px] lg:w-[21.5px]">
               <img
@@ -157,7 +109,7 @@ const Sidebar = ({ onValueChange }) => {
                       : ''
                   }/images/logo/pythia.svg`}
                   alt="image"
-                  className="w-[40px] lg:w-[300px]"
+                  className={`w-[40px] lg:w-[300px] ${isOpen ? '' : 'hidden'}`}
                 />
               </a>
             </div>
