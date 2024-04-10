0a51ff3b8b2d66d9ac198b6f81c8eb4daf0bd67c
Bruno Santos <brunolsantos152@gmail.com>
2024-04-10 13:13:15 -0300

    feat: changes

--- utils/api-pythia.tsx
+++ utils/api-pythia.tsx
@@ -26,7 +26,7 @@ export async function getUserChats(userSessionToken: string) {
 
 export async function createUserChat(data: any, userSessionToken: string) {
   const config = {
-    method: 'post' as 'post',
+    method: 'put' as 'put',
     url: `${process.env.NEXT_PUBLIC_API_BACKEND_BASE_URL}/pythia/functions/createUserChat`,
     headers: {
       'x-parse-application-id': `${process.env.NEXT_PUBLIC_API_BACKEND_KEY}`,
