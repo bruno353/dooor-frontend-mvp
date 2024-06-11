5d124f21c06ad9235aa4985179d284b5f7a2a2fc
Divyam-Garg <41555489+Divyam-Garg@users.noreply.github.com>
2024-06-11 15:06:39 +1000

    made charts prettier

--- components/DynamicChart/index.tsx
+++ components/DynamicChart/index.tsx
@@ -151,13 +151,13 @@ const DynamicChart = ({ data, xkey, ykey }) => {
 
     return (
         
-        <ResponsiveContainer width="100%" height={200}>
+        <ResponsiveContainer width="90%" height={200}>
             <LineChart
                 data={data}
                 margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
                 >
                 <CartesianGrid strokeDasharray="" />
-                <XAxis dataKey={xkey} />
+                <XAxis dataKey={xkey} tick={{ fontSize: 8 }} tickCount={data.length} interval={0}/>
                 <YAxis dataKey={ykey} />
                 <Tooltip />
                 {/* <Legend verticalAlign="top" wrapperStyle={{ lineHeight: "40px" }} /> */}
