677f0b449043556ae26cc08d36a424c3b6accf18
Bruno Santos <brunolsantos152@gmail.com>
2024-12-24 11:19:53 -0300

    feat: new interaction wallet

--- components/Chat/index.tsx
+++ components/Chat/index.tsx
@@ -1,22 +1,5 @@
 'use client'
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable no-unused-vars */
 
-import Footer from '../Footer'
-import { useEffect, useState, useContext, useRef } from 'react'
-import dynamic from 'next/dynamic'
-import 'react-quill/dist/quill.snow.css' // import styles
-import './react-quill.css'
-import nookies, { parseCookies, setCookie } from 'nookies'
-import {
-  getUserChat,
-  inputUserChatMessage,
-  insertBadFeedback,
-} from '@/utils/api-pythia'
-import { AccountContext } from '@/contexts/AccountContext'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { getSanitizeText } from '@/utils/functions-chat'
 import { ChatComponent } from '../PythiaLandingPage/ChatComponent'
 
 const ChatPage = (id: any) => {
